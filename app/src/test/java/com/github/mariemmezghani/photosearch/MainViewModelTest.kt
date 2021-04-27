8f77971570cd0998b55e269689c901839eb2aa2e
MariemMezghani <35550711+MariemMezghani@users.noreply.github.com>
2021-04-27 14:11:17 +0200

    update

--- app/src/test/java/com/github/mariemmezghani/photosearch/MainViewModelTest.kt
+++ app/src/test/java/com/github/mariemmezghani/photosearch/MainViewModelTest.kt
@@ -1,6 +1,10 @@
 package com.github.mariemmezghani.photosearch
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import com.github.mariemmezghani.photosearch.model.Photo
 import com.github.mariemmezghani.photosearch.network.ApiService
 import org.junit.Assert.*
 import org.junit.Rule
@@ -9,17 +13,22 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
+import java.util.*
 
 @RunWith(JUnit4::class)
 class MainViewModelTest{
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Mock
     lateinit var mainViewModel: MainViewModel
+    @Mock
     lateinit var repository:PhotosRepository
+    @Mock
     val api:ApiService= ApiService.create()
 
 
@@ -31,11 +40,26 @@ class MainViewModelTest{
     }
 
     @Test
-    fun getPhotosTest(){
+    fun getPhotosTest_NotNull(){
         //Invoke
         mainViewModel.getPhotosList("nature")
         // verify
         assertNotNull(this.mainViewModel.photos)
+    }
+    @Test
+    fun getPhotosTest(){
+        // Mock API response
+        Mockito.`when`(repository.getPhotos(ArgumentMatchers.anyString())).thenAnswer {
+            return@thenAnswer (Maybe.just(ArgumentMatchers.anyList<Photo>()))
+        }
+        // Attacch fake observer
+        val observer = mock(Observer::class.java) as Observer<LiveData<PagingData<Photo>>>
+        this.mainViewModel.photos.observeForever(observer)
+
+        //Invoke
+        mainViewModel.getPhotosList(ArgumentMatchers.anyString())
+        // verify
+        assertEquals(LiveData<PagingData<Photo>>.Status.SUCCESS, this.mainViewModel.photos?.value.status)
     }
 
 }
